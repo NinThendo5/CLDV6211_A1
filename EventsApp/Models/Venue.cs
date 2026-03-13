a7ea2c1f4cf5d22ca67aaf1edc990761c0201508
NinThendo5 <st10446417@vcconnect.edu.za>
2026-03-13 13:45:10 +0200

    Added key and variables in venue class

--- EventsApp/Models/Venue.cs
+++ EventsApp/Models/Venue.cs
@@ -1,6 +1,16 @@
-﻿namespace EventsApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventsApp.Models
 {
     public class Venue
     {
+        [Key]
+
+        public int VenueId { get; set; }
+
+        public string VenueName { get; set; }
+
+        public string Location { get; set; }
+
     }
 }
